9639bc653278c6ef82348a9cf06de351ad420195
Ravi Raj <rrrravi234@gmail.com>
2022-02-08 04:44:19 +0530

    Adding Animation
    Adding Animation to player and Enemy.

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Background : MonoBehaviour
-{
+{  // Rendrer is used for calling meshrendrer:
     public Renderer meshRend;
     public float speed;
 
     // Update is called once per frame
     void Update()
     {
+        // scrolling of background
+
         Vector2 offset = meshRend.material.mainTextureOffset;
         offset = offset + new Vector2(0, speed * Time.deltaTime);
         meshRend.material.mainTextureOffset = offset;
