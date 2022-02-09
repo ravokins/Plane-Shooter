33cb8583501bb61eafdc9cd27eb59ecb5dd2eeaf
Ravi Raj <rrrravi234@gmail.com>
2022-02-09 14:08:12 +0530

    Adding clamp:
    This  help our player to stay in screen .

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,11 @@ public class PlayerMovement : MonoBehaviour
 {
 
     float speed = 10f;
+    float Padding = 0.8f;
+    float min_X;
+    float max_X;
+    float min_Y;
+    float max_Y;
 
     // Start is called before the first frame update
 
@@ -19,13 +24,22 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         Movement();
+        FindBoundaries();
+    }
+    void FindBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        min_X = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x+Padding;
+        max_X = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x-Padding;
+        min_Y = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y+Padding;
+        max_Y = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y-Padding;
     }
     private void Movement()
     {  // For Horizontal & Vertical Movement:
         float HorizontalInput = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         float VerticalInput = Input.GetAxis("Vertical") * speed * Time.deltaTime; // This is for vertical movement: 
-        float newXpos = transform.position.x + HorizontalInput;
-        float newYpos = transform.position.y + VerticalInput;
+        float newXpos = Mathf.Clamp( transform.position.x + HorizontalInput,min_X,max_X);
+        float newYpos =Mathf.Clamp( transform.position.y + VerticalInput,min_Y,max_Y);
         transform.position = new Vector2(newXpos, newYpos);
 
         // for vrtical movement:
